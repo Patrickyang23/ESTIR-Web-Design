6a7b68d4a26a85effa0e8c077dd62833b441b746
Yikun Yang <yikuny@umich.edu>
2024-11-16 15:16:57 -0500

    Add index.html

---
+++ js/analytics.js
@@ -0,0 +1,8 @@
+// Google Analytics setup
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag('js', new Date());
+
+gtag('config', 'G-YW5FGNQDL2');
